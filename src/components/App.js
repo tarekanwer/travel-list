e57cbcf1819ee8c8a182b09992ecf91b344e1eec
tarekanwer <tito.a7mad@gmail.com>
2024-04-10 19:08:53 +0200

    travel app finished

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+import Form from "./Form";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearItems() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items ?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearITems={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,5 @@
+function Logo() {
+  return <h1> 💼 Far Away 🌴</h1>;
+}
+
+export default Logo;
